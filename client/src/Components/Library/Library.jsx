6358045d29135d3e97290370966ad1a13866acd8
Mahmudul-Hasan-G <shantohasan1983@gmail.com>
2023-11-29 15:04:26 +0100

    something

---
+++ client/src/Components/Library/Library.jsx
@@ -0,0 +1,11 @@
+
+
+const Library = () => {
+    return (
+        <div>
+            <h1>It is library page</h1>
+        </div>
+    );
+};
+
+export default Library;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,16 +3,39 @@ import ReactDOM from 'react-dom/client'
 import App from './App';
 import './index.css'
 
+
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Home from './Components/Home/Home.jsx'
+import Library from './Components/Library/Library';
+import Insert from './Components/Insert/Insert';
+import LogIn from './Components/LogIn/LogIn';
 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/library",
+        element: <Library />,
+      },
+      {
+        path: "/insert",
+        element: <Insert />,
+      },
+      {
+        path: "/logIn",
+        element: <LogIn />,
+      },
+    ]
   },
 ]);
 
