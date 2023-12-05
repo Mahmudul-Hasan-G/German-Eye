67c3c92ccc74ab8ec38838ea400460a67f758fb7
anu muley <annusannu@gmail.com>
2023-12-05 10:06:29 +0100

    updated

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,9 +1,11 @@
-
+import Marquee from "react-fast-marquee";
 
 const Home = () => {
     return (
         <div>
-            <h1>this is the home page</h1>
+           <Marquee speed={80} direction="right" gradient={true} gradientColor={[239, 23, 23]}>
+                <h1 className="text-6xl mb-4 p-2">Cultural places and Weather Updates</h1>
+            </Marquee>
         </div>
     );
 };
