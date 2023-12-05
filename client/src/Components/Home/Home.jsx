7184a55dbcc65cccc80d7dc129503f4dfccaf3d3
anu muley <annusannu@gmail.com>
2023-12-05 10:19:58 +0100

    updated

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,18 +1,14 @@
 
 import Marquee from "react-fast-marquee";
 
-const Home = () => {
-    return (
-        <div>
-           <Marquee speed={80} direction="right" gradient={true} gradientColor={[239, 23, 23]}>
-                <h1 className="text-6xl mb-4 p-2">Cultural places and Weather Updates</h1>
-            </Marquee>
-
-
 
 const Home = () => {
   return (
     <div>
+ <Marquee speed={80} direction="right" gradient={true} gradientColor={[239, 23, 23]}>
+                <h1 className="text-6xl mb-4 p-2">Cultural places and Weather Updates</h1>
+            </Marquee>
+
       <form className="hero min-h-screen" style={{ backgroundImage: `url(/backgroungimg-germany.png)` }}>
         <div className="bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
