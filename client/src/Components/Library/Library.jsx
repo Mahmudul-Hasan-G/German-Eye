4f5374ae4540d21ccd8dae8adfcadcde396e7a13
Jemila Kaffaf <jemila.1997@outlook.it>
2023-12-07 10:20:53 +0100

    changed styling of library

--- client/src/Components/Library/Library.jsx
+++ client/src/Components/Library/Library.jsx
@@ -3,6 +3,10 @@
 const Library = () => {
     return (
         <div>
+        <h1 className="text-center text-4xl font-bold mb-6 text-yellow-500 border-b-2 border-red-500 rounded-full px-2 py-1">
+        Library
+      </h1>
+            <h2 className="text-3xl font-bold mb-6">Phylosophy</h2>
      <div className="flex space-x-20">
       {[...Array(4)].map((_, index) => (
         <div key={index} className="card w-96 bg-base-100 shadow-xl">
@@ -14,16 +18,22 @@ const Library = () => {
             />
           </figure>
           <div className="card-body items-center text-center">
-            <h2 className="card-title">Philosophy</h2>
-            <p>example books</p>
+            <h2 className="card-title">Title of the book</h2>
+            <p>a small bio of what the books talk about</p>
             <div className="card-actions">
-              <button className="btn btn-primary">Read me now!</button>
-            </div>
+  <button
+    className="btn btn-primary"
+    style={{ backgroundColor: 'red', color: 'white' }}
+  >
+    Read me now!
+  </button>
+</div>
           </div>
         </div>
       ))}
     </div>
 
+    <h2 className="text-3xl font-bold mb-6">Horror</h2>
     <div className="flex space-x-20">
       {[...Array(4)].map((_, index) => (
         <div key={index} className="card w-96 bg-base-100 shadow-xl">
@@ -35,16 +45,22 @@ const Library = () => {
             />
           </figure>
           <div className="card-body items-center text-center">
-            <h2 className="card-title">Horror</h2>
-            <p>example books</p>
+            <h2 className="card-title">Title of the book</h2>
+            <p>a small bio of what the book talk about</p>
             <div className="card-actions">
-              <button className="btn btn-primary">Read me now!</button>
-            </div>
+  <button
+    className="btn btn-primary"
+    style={{ backgroundColor: 'red', color: 'white' }}
+  >
+    Read me now!
+  </button>
+</div>
           </div>
         </div>
       ))}
     </div>
 
+    <h2 className="text-3xl font-bold mb-6">Fantasy</h2>
     <div className="flex space-x-20">
       {[...Array(4)].map((_, index) => (
         <div key={index} className="card w-96 bg-base-100 shadow-xl">
@@ -56,11 +72,16 @@ const Library = () => {
             />
           </figure>
           <div className="card-body items-center text-center">
-            <h2 className="card-title">Fantasy</h2>
-            <p>example books</p>
+            <h2 className="card-title">Title of the book</h2>
+            <p>a small bio of what the book talk about</p>
             <div className="card-actions">
-              <button className="btn btn-primary">Read me now!</button>
-            </div>
+  <button
+    className="btn btn-primary"
+    style={{ backgroundColor: 'red', color: 'white' }}
+  >
+    Read me now!
+  </button>
+</div>
           </div>
         </div>
       ))}
