ff919377bef2cc6f997790d44b62431026809d90
Jemila Kaffaf <jemila.1997@outlook.it>
2023-12-11 10:46:41 +0100

    added the -1 page function

--- client/src/Components/PlaceDetail/PlaceDetail.jsx
+++ client/src/Components/PlaceDetail/PlaceDetail.jsx
@@ -1,11 +1,16 @@
 import { useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 
 const PlaceDetail = () => {
     const location = useLocation();
+    const navigate = useNavigate();
     const { place } = location.state || {};
     const { city, address, zipCode, image, placeName, description } = place || {};
 
+    const handleClick = () => {
+        navigate(-1);
+    }
     return (
         <div>
             <div className="card card-compact w-100% bg-base-100 shadow-xl">
@@ -17,7 +22,7 @@ const PlaceDetail = () => {
                     <h1>{placeName}</h1>
                     <p>{description}</p>
                     <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+                        <button onClick={handleClick} className="btn btn-primary px-10">Go back</button>
                     </div>
                 </div>
             </div>
