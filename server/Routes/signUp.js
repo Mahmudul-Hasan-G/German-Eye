ed87c1f5b27cb71cfd70a691a83b09b4ce798824
anu muley <annusannu@gmail.com>
2023-12-11 11:30:43 +0100

    updated

--- server/Routes/signUp.js
+++ server/Routes/signUp.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import User from '../Model/UserSchema.js';
-
+import bcrypt from 'bcryptjs';
 const router = express.Router();
 
 router.post('/signup', async (req, res) => {
@@ -9,6 +9,10 @@ router.post('/signup', async (req, res) => {
     const { name, email, city, password, cPassword } = req.body;
     try {
         const user = new User({ name, email, city, password, cPassword });
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+        user.cPassword = await bcrypt.hash(cPassword, salt);
 
         await user.save();
     }
