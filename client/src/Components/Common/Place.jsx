f3532818d0a2c4b3d99dc17dd23260921f90a741
Jemila Kaffaf <jemila.1997@outlook.it>
2023-12-11 09:35:21 +0100

    places has been updated

--- client/src/Components/Common/Place.jsx
+++ client/src/Components/Common/Place.jsx
@@ -8,12 +8,18 @@ const Place = ({ place }) => {
     const { city, address, zipCode, placeName, image } = place || {};
     return (
         <div>
-            <img width="20%" src={`${image}`} />
-            <h1>{image}</h1>
-            <h1>{city}</h1>
-            <h1>{address}</h1>
-            <h1>{zipCode}</h1>
-            <h1>{placeName}</h1>
+            <div className="flex space-x-4 card lg:card-side bg-base-100 shadow-xl w-80">
+  <figure className="w-full">
+    <img className="w-40 h-48" src={image} alt="Album" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">{city}</h2>
+    <p>{address}</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Bio!</button>
+    </div>
+  </div>
+  </div>
         </div>
     );
 };
