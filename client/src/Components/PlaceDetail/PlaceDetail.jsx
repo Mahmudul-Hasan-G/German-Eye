5e10a90421eb1dd4e576162624823eef426cd154
Mahmudul-Hasan-G <shantohasan1983@gmail.com>
2023-12-18 23:46:40 +0100

    change

--- client/src/Components/PlaceDetail/PlaceDetail.jsx
+++ client/src/Components/PlaceDetail/PlaceDetail.jsx
@@ -16,29 +16,27 @@ const PlaceDetail = () => {
 
     return (
         <div className="bg-red-500">
-jemila14
+
             <div className="px-6 pt-20 pb-6">
-            <div className="bg-white grid grid-cols-2 gap-4 justify-items-center px-6 py-6">
-                <div className="position-start">
-                    <h2 className="text-6xl">{city}</h2>
-                    <p>{address}</p>
-                    <p>{zipCode}</p>
-                    <h1>{placeName}</h1>
-                    <p>{description}</p>
-            
+                <div className="bg-white grid grid-cols-2 gap-4 justify-items-center px-6 py-6">
+                    <div className="position-start">
+                        <h2 className="text-6xl">{city}</h2>
+                        <p>{address}</p>
+                        <p>{zipCode}</p>
+                        <h1>{placeName}</h1>
+                        <p>{description}</p>
+
                         <button onClick={handleClick} className="btn btn-accent px-10">Go back</button>
                     </div>
                     <div className="">
                         <img src={image} alt="Photo" />
                     </div>
-            </div>
- jemila14
-                <div>
-                <img src={image} alt="Photo" />
                 </div>
-            </div>
+
 
             </div>
+
+
 
         </div>
     );
