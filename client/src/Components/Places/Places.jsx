fa17d8cbc7470295c2ae5d1d296849a4aed88d1b
Mahmudul-Hasan-G <shantohasan1983@gmail.com>
2023-12-29 15:35:59 +0100

    change

--- client/src/Components/Places/Places.jsx
+++ client/src/Components/Places/Places.jsx
@@ -1,33 +1,36 @@
-import { useEffect, useState } from "react";
+//import { useEffect, useState } from "react";
 import Place from "../Common/Place.jsx";
-import axios from "axios";
+//import axios from "axios";
+import { useAuth } from "../Common/authContext.jsx";
 
 
 const Places = () => {
-    const [places, setPlaces] = useState([]);
+const {allPlaces} = useAuth();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get('http://localhost:5000/places');
-                console.log(response.data);
-                setPlaces(response.data);
+    // const [places, setPlaces] = useState([]);
 
-            } catch (error) {
-                console.error(error.message);
-            }
-        };
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         try {
+    //             const response = await axios.get('http://localhost:5000/places');
+    //             console.log(response.data);
+    //             setPlaces(response.data);
 
-        fetchData();
+    //         } catch (error) {
+    //             console.error(error.message);
+    //         }
+    //     };
 
-    }, []);
+    //     fetchData();
+
+    // }, []);
 
 
     return (
         <div className="grid grid-cols-4 gap-6 text-xl">
 
             {
-                places.map(place => <Place
+                allPlaces.map(place => <Place
                     key={place._id}
                     place={place}
                 />)
