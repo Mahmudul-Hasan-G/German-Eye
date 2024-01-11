d18fce73efaf3fe93e169dc49202f7fdcd9e3c1b
Mahmudul-Hasan-G <shantohasan1983@gmail.com>
2024-01-11 10:17:09 +0100

    change

--- client/src/Components/AddDetail/AddDetail.jsx
+++ client/src/Components/AddDetail/AddDetail.jsx
@@ -6,7 +6,7 @@ const AddDetail = ({ add }) => {
     return (
         <div className=" w-full border-solid border-2 border-indigo-600">
 
-            <img className='h-full' src={image} alt="" />
+            <img className='h-full w-full' src={image} alt="" />
         </div>
     );
 };
